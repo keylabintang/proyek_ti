b0268235eb364ea2e9907ff50ff710a11bd48b9f
Ferdi Ananta Salman <ferdiananta51@gmail.com>
2023-10-21 23:05:52 +0700

    test5

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::resource('/pendaftaran', PendaftaranController::class);
 
 Route::resource('/member', MemberController::class);
 
-// test
+// test1
 
 Route::get('/dashboard', function () {
     return view('admin.home-admin');
